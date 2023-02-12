8862c43bedbce16ff771446825d8debd78e0fa92
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-11 16:11:25 -0800

    saved

--- server/middlewares/JWT/adminAuth.ts
+++ server/middlewares/JWT/adminAuth.ts
@@ -1,1 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { adminDetails } from "../../model/allInterfaces";
+
+export const isAdmin = (
+  req: adminDetails,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user;
+  if (user && user.role === "admin") {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ message: "You are not authorized to perform this action" });
+  }
+};
