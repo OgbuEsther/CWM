b1bf44f1df153acfa612f9b0f98ed5cc254b90da
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-11 16:12:54 -0800

    saved

--- server/model/allInterfaces.ts
+++ server/model/allInterfaces.ts
@@ -1,37 +1,38 @@
 export interface clientDetails {
-    name : string;
-    address : string;
-    clientType : boolean;
-    email : string;
-    phoneNumber : string;
-    dashBoard : {}[];
-    password : string
+  name: string;
+  address: string;
+  clientType: boolean;
+  email: string;
+  phoneNumber: string;
+  dashBoard: {}[];
+  password: string;
 }
 
 export interface clientDashBoard {
-    message :  {}[];
-    paymentLog : {}[];
-    bills : string;
-    schedule : string
+  message: {}[];
+  paymentLog: {}[];
+  bills: string;
+  schedule: string;
 }
 
-
-export interface adminDetails{
-name : string;
-email : string;
-password : string;
-dashBoard : {}[];
+export interface adminDetails {
+  name: string;
+  email: string;
+  password: string;
+  dashBoard: {}[];
 }
 
 export interface adminDashboard {
-message : {}[];
-paymentLog : {}[];
-approvals : boolean;
-viewArea: string;
-expectedPayment : string;
-
+  message: {}[];
+  paymentLog: {}[];
+  approvals: boolean;
+  viewArea: string;
+  expectedPayment: string;
 }
 
-
+export interface IAuthUser extends Request {
+  user: clientDetails;
+  admin: adminDetails;
+}
 
 //socket.io
